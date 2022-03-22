7eff02d7bee6d0c12a79daaa9316b210957eab71
jayw1112 <housexpresso@gmail.com>
2022-03-21 18:41:56 -0700

    Added product page and dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,43 @@
-import Head from 'next/head'
-import { Typography } from '@mui/material'
+import { Alert, CircularProgress, Grid, Typography } from '@mui/material'
 import Layout from '../components/Layout'
+import ProductItem from '../components/ProductItem'
+import client from '../utils/client'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
-  return <Layout>List Products</Layout>
+  const [state, setState] = useState({
+    products: [],
+    error: '',
+    loading: true,
+  })
+  const { loading, error, products } = state
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const products = await client.fetch(`*[_type == "product"]`)
+        setState({ products, loading: false })
+      } catch (error) {
+        setState({ loading: false, error: err.message })
+      }
+    }
+    fetchData()
+  }, [])
+  return (
+    <Layout>
+      {loading ? (
+        <CircularProgress />
+      ) : error ? (
+        <Alert variant='danger'>{error}</Alert>
+      ) : (
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item md={4} key={product.slug}>
+              <ProductItem product={product}></ProductItem>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Layout>
+  )
 }
