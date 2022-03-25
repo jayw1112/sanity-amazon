03532436b9ff90d9ce34fa8553734e4a3a4d64fc
jayw1112 <housexpresso@gmail.com>
2022-03-24 19:45:50 -0700

    cart button added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import { Alert, CircularProgress, Grid, Typography } from '@mui/material'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { useSnackbar } from 'notistack'
 import Layout from '../components/Layout'
 import ProductItem from '../components/ProductItem'
 import client from '../utils/client'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { urlForThumbnail } from '../utils/image'
+import { Store } from '../utils/Store'
 
 export default function Home() {
+  const {
+    state: { cart },
+    dispatch,
+  } = useContext(Store)
+  const router = useRouter()
+  const { enqueueSnackbar } = useSnackbar()
   const [state, setState] = useState({
     products: [],
     error: '',
@@ -23,6 +34,33 @@ export default function Home() {
     }
     fetchData()
   }, [])
+
+  const addToCartHandler = async (product) => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id)
+    const quantity = existItem ? existItem.quantity + 1 : 1
+    const { data } = await axios.get(`/api/products/${product._id}`)
+    if (data.countInStock < quantity) {
+      enqueueSnackbar('Sorry. Product is out of stock', { variant: 'error' })
+      return
+    }
+    dispatch({
+      type: 'CART_ADD_ITEM',
+      payload: {
+        _key: product._id,
+        name: product.name,
+        countInStock: product.countInStock,
+        slug: product.slug.current,
+        price: product.price,
+        image: urlForThumbnail(product.image),
+        quantity,
+      },
+    })
+    enqueueSnackbar(`${product.name} added to the cart`, {
+      variant: 'success',
+    })
+    router.push('/cart')
+  }
+
   return (
     <Layout>
       {loading ? (
